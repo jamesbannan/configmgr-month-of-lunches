f0ba2438974b20d69cd8997a2c7e95d163f0c697
jamesbannan <james@bannan.com.au>
2016-01-28 21:48:55 +1100

    Updated AD Sites and Services script

--- HydrationConfigMgr/Source/Hydration/Applications/Configure - Create AD Subnets/Configure-CreateADSubnets.ps1
+++ HydrationConfigMgr/Source/Hydration/Applications/Configure - Create AD Subnets/Configure-CreateADSubnets.ps1
@@ -3,8 +3,8 @@ Solution: Hydration
 Purpose: Used to create AD Sites and Subnets
 Version: 1.2 - January 10, 2013
 
-This script is provided "AS IS" with no warranties, confers no rights and 
-is not supported by the authors or Deployment Artist. 
+This script is provided "AS IS" with no warranties, confers no rights and
+is not supported by the authors or Deployment Artist.
 
 Author - Johan Arwidmark
     Twitter: @jarwidmark
@@ -12,24 +12,24 @@ Author - Johan Arwidmark
 #>
 
 
-# Determine where to do the logging 
-$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment 
-$logPath = $tsenv.Value("LogPath") 
-$logFile = "$logPath\$($myInvocation.MyCommand).log" 
+# Determine where to do the logging
+$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+$logPath = $tsenv.Value("LogPath")
+$logFile = "$logPath\$($myInvocation.MyCommand).log"
 
-# Start the logging 
-Start-Transcript $logFile 
-Write-Host "Logging to $logFile" 
+# Start the logging
+Start-Transcript $logFile
+Write-Host "Logging to $logFile"
 
 # Create Empty AD Sites (sites with no domain controllers, for lab purpose only)
+New-ADReplicationSite -Name NewYork
 New-ADReplicationSite -Name Stockholm
 New-ADReplicationSite -Name Liverpool
 
-# Create AD Subnets 
+# Create AD Subnets
 New-ADReplicationSubnet -Name "192.168.21.0/24" -Site NewYork
 New-ADReplicationSubnet -Name "192.168.22.0/24" -Site Stockholm
 New-ADReplicationSubnet -Name "192.168.23.0/24" -Site Liverpool
 
-# Stop logging 
+# Stop logging
 Stop-Transcript
-
