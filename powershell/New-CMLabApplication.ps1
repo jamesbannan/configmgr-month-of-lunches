3287dcd4b215db951861846dd9d2dd608ed3e3a6
James Bannan <james@bannan.com.au>
2016-01-30 13:51:14 +1100

    Paint.NET version change in New-CMLabApplication.ps1

--- powershell/New-CMLabApplication.ps1
+++ powershell/New-CMLabApplication.ps1
@@ -3,7 +3,7 @@
 $CMSiteCode = 'PS1'
 $CMSiteServer = 'CM01.mol.sccmlab.net'
 $CMNameSpace = "root\SMS\site_$CMSiteCode"
-$ApplicationName = 'Paint.NET 4.0.5'
+$ApplicationName = 'Paint.NET 4.0.9'
 
 # 1.0 Create new Application
 
@@ -16,15 +16,15 @@ $Application = Get-CMApplication -Name $ApplicationName
 Add-CMDeploymentType -ApplicationName $Application.LocalizedDisplayName `
   -AutoIdentifyFromInstallationFile `
   -ForceForUnknownPublisher $true `
-  -InstallationFileLocation '\\CM01\Sources\Software\Paint.NET\4.0.5\x64\PaintDotNet.x64.msi' `
+  -InstallationFileLocation '\\CM01\Sources\Software\Paint.NET\4.0.9\x64\PaintDotNet.x64.msi' `
   -MsiInstaller `
-  -DeploymentTypeName 'Paint.NET 4.0.5 (x64) - Windows Installer (*.msi file)'
+  -DeploymentTypeName 'Paint.NET 4.0.9 (x64) - Windows Installer (*.msi file)'
 
 # 1.2 Create x86 Deployment Type
 
 Add-CMDeploymentType -ApplicationName $Application.LocalizedDisplayName `
   -AutoIdentifyFromInstallationFile `
   -ForceForUnknownPublisher $true `
-  -InstallationFileLocation '\\CM01\Sources\Software\Paint.NET\4.0.5\x86\PaintDotNet.x86.msi' `
+  -InstallationFileLocation '\\CM01\Sources\Software\Paint.NET\4.0.9\x86\PaintDotNet.x86.msi' `
   -MsiInstaller `
-  -DeploymentTypeName 'Paint.NET 4.0.5 (x86) - Windows Installer (*.msi file)'
+  -DeploymentTypeName 'Paint.NET 4.0.9 (x86) - Windows Installer (*.msi file)'
