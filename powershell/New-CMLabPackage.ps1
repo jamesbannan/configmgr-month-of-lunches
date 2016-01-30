337b8daa4a185297c97a0c265fc7ddca9e0ee751
James Bannan <james@bannan.com.au>
2016-01-30 14:12:48 +1100

    Fixed $CMNameSpace in New-CMLabPackage.ps1

--- powershell/New-CMLabPackage.ps1
+++ powershell/New-CMLabPackage.ps1
@@ -2,18 +2,18 @@
 
 $CMSiteCode = 'PS1'
 $CMSiteServer = 'CM01.mol.sccmlab.net'
-$CMNameSpace = "rootSMSsite_$CMSiteCode"
-$PackageName = 'Notepad++ 6.8'
+$CMNameSpace = "root\SMS\site_$CMSiteCode"
+$PackageName = 'Notepad++ 6.8.9'
 
 # 1.0 Create new Package
 
-New-CMPackage -Name $PackageName -Path '\\cm01\Sources\Software\Notepad++ 6.8'
+New-CMPackage -Name $PackageName -Path '\\cm01\Sources\Software\Notepad++ 6.8.9'
 
 # 1.1 Create new Program
 
 New-CMProgram -PackageName $PackageName
   -StandardProgramName 'Install - Notepad++ 6.8'
-  -CommandLine 'npp.6.8.Installer.exe /S'
+  -CommandLine 'npp.6.8.9.Installer.exe /S'
   -RunType Hidden
   -RunMode RunWithAdministrativeRights
   -UserInteraction $false
